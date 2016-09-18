74f8cfa55a303cc2d504c2c07131877022299c78
Will Landau <will.landau@gmail.com>
2016-09-17 23:26:47 -0400

    No need to set global option downsize on load

--- R/zzz.R
+++
@@ -1,4 +0,0 @@
-# Execute when the package loads.
-.onLoad = function(libname, pkgname){
-  ds_options()
-}
--- tests/testthat/test-ds_options.R
+++
@@ -1,7 +0,0 @@
-# library(testthat); library(downsize)
-context("ds_options")
-
-test_that("Function ds_options() runs correctly", {
-  expect_null(ds_options())
-  expect_false(getOption("downsize"))
-})
